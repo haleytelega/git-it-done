197b1ff44847a8dae881ccd6aa70342e1776c48c
Haley Telega <haleytelega@outlook.com>
2022-03-23 13:20:59 -0400

    added fetch statement

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,14 @@
-var getUserRepos = function () {
-    console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos")
-}; 
+var getUserRepos = function (user) {
+    //format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
+    //make a requst to the url
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
+    console.log("outside") //run bc not dependant on anything
+}
 getUserRepos();
